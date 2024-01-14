0b439feccc09cb0cd45a642851b3fed7332e7844
Margarita Ivantsova <148205607+IIMargo@users.noreply.github.com>
2024-01-14 17:53:41 +0300

    Commit hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,8 +1,13 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.util.List;
+
+import static jm.task.core.jdbc.util.Util.*;
 
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
@@ -12,31 +17,56 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        try (Session session = getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery(CREATE_TABLE).executeUpdate();
+            transaction.commit();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try (Session session = getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery(DROP_TABLE).executeUpdate();
+            transaction.commit();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.save(user);
+            transaction.commit();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.delete(session.get(User.class, id));
+            transaction.commit();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (Session session = getSessionFactory().openSession()) {
+            Query<User> query = session.createQuery("from User", User.class);
+            query.stream().forEach(System.out::println);
+            return query.list();
+        }
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try (Session session = getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createQuery("delete from User").executeUpdate();
+            transaction.commit();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,12 +1,20 @@
 package jm.task.core.jdbc.util;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static final String DRIVER = "org.postgresql.Driver";
     public static final String DB = "Task_1.1.3";
     public static final String URL = "jdbc:postgresql://localhost:5432/" + DB;
+    public static final String DIALECT = "org.hibernate.dialect.PostgreSQLDialect";
     public static final String USER = "postgres";
     public static final String PASSWORD = "rootroot";
     public static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS users (Id SERIAL PRIMARY KEY, name VARCHAR, lastName varchar, age integer)";
@@ -16,5 +24,21 @@ public class Util {
     public static final String CLEAN = "DELETE FROM users";
     public static Connection getConn() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+    public static SessionFactory getSessionFactory() {
+        Properties properties = new Properties();
+        properties.put(Environment.DRIVER, DRIVER);
+        properties.put(Environment.URL, URL);
+        properties.put(Environment.USER, USER);
+        properties.put(Environment.PASS, PASSWORD);
+        properties.put(Environment.DIALECT, DIALECT);
+        properties.put(Environment.SHOW_SQL, "true");
+        properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+        SessionFactory sessionFactory = new Configuration()
+                .setProperties(properties)
+                .addAnnotatedClass(User.class)
+                .buildSessionFactory();
+        return sessionFactory;
     }
 }
